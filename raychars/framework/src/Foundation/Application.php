f65d36c4d434bb3602bf31d05695a487e60d175a
raychars <shxhx00@126.com>
2018-04-05 22:21:44 +0800

    framework file

---
+++ raychars/framework/src/Foundation/Application.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiandutianxia
+ * Date: 2018/4/5
+ * Time: 下午9:39
+ */
+namespace Raychars\Foundation;
+
+class Application{
+    public function index(){
+        echo "sdfsd";
+    }
+}
